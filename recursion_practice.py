531cf509e184505f0e49f5ccb4a043f8b19059cf
Corey <jcshott@gmail.com>
2015-10-14 10:31:00 -0700

    some more recursion practice

--- recursion_practice.py
+++ recursion_practice.py
@@ -16,3 +16,30 @@ print len_list([7, 8, 5, 8])
 #that is returned up to another 1 + 1 = 2 (c)
 #that is returned up to another 1 + 2 = 3 (b)
 #that is returned up to another 1 + 3 (a)
+
+def reverse_string(astring):
+	"""use recursion to reverse a string"""
+
+	if len(astring) < 1:
+		return astring
+	else:
+		return reverse_string(astring[1:]) + astring[0]
+
+print reverse_string("joy")
+print reverse_string("Corey")
+print reverse_string("Ruby Roo")
+
+def is_pal(s):
+	"""checks whether something is a palindrome using recursion. 
+	takes a string of all alpha numeric characters (no whitespace or special chars in same case)
+	returns True if is a palindrome, False if not.
+	could use helper functions if wanted to take other types of strings (i.e. phrases or upper/lower combinations"""
+
+	if len(s) < 2:
+		return True
+	else:
+		return is_pal(s[1:-1]) and s[0] == s[-1]
+	return False
+
+print is_pal("radar")
+print is_pal("corey")
