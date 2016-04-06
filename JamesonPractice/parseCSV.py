5a415fb0afa03757795f30edc3611913ac0afd74
Corey Shott <jcshott@gmail.com>
2016-04-06 15:07:17 -0700

    i think i have accounting for escaped quotes too though the if/elif stmts are getting a bit specific to match test cases

--- JamesonPractice/parseCSV.py
+++ JamesonPractice/parseCSV.py
@@ -13,42 +13,58 @@ def parse_csv(text):
 	quote = False
 
 	while curr_idx < len(text):
+		if curr_idx == 0 and text[curr_idx] == "\"":
+			quote = True
+			curr_idx += 1
 		# if we see a quote, we need to ignore commas until we see another so set to True
-		if text[curr_idx] == "\"" and not quote:
+		elif text[curr_idx] == "\"" and text[curr_idx-1] != "\\" and not quote:
 			quote = True
 			curr_idx += 1
 		# if we've seen a quote already, we have our second "escape" quote so return to false so we catch next comma as seperator
-		elif text[curr_idx] == "\"" and quote:
+		elif text[curr_idx] == "\"" and text[curr_idx-1] != "\\" and quote:
 			quote = False
 			curr_idx +=1
+		elif text[curr_idx] == "\"" and text[curr_idx-1] == "\\":
+			curr_idx += 1
 
 		# if we hit a comma and we don't have a quote/it's not within an escape, print the string so far
 		elif text[curr_idx] == "," and not quote:
-			print text[beg_idx:curr_idx].replace("\",\"", ",").strip().strip("\"")
+			# strip whitespace and extra quotes first then get rid of escape symbol and turn escaped comma into simple comma
+			print text[beg_idx:curr_idx].strip().strip("\"").replace("\\", "").replace("\",\"", ",")
 			beg_idx = curr_idx+1
 			curr_idx = beg_idx
 		
 		elif text[curr_idx] == "," and quote:
-				curr_idx += 1
+			curr_idx += 1
 		else:
 			curr_idx += 1
 
-	out = text[beg_idx:].replace("\",\"", ",")
-	print out.strip()
+	print text[beg_idx:].strip().strip("\"").replace("\\", "").replace("\",\"", ",")
 
 if __name__ == '__main__':
-	## input: 
-	#rocky,ruby","dog,"one,two",yes
-	#escape, all, the things
-	# output in terminal:
-		# rocky
-		# ruby,dog
-		# one,two
-		# yes
-		# escape
-		# all
-		# the things
+	## my example input: 
+	# parse, all, the things
+	# rocky, ruby","dog,"one,two",yes
+	# A, "B,C", "D\",", E
+	# "\"G, H", l, m\"
+
+	# my understand of what the output in terminal should be:
+	# parse
+	# all
+	# the things
+	# rocky
+	# ruby,dog
+	# one,two
+	# yes
+	# A
+	# B,C
+	# D",
+	# E
+	# "G, H
+	# l
+	# m
 
 	f_info = open("test.csv")
-
-	parse_csv(f_info.read())
+	
+	for line in f_info:
+		parse_csv(line)
