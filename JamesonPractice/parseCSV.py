38af0c0bc65309c78135cead98a3a64205387bbc
Corey Shott <jcshott@gmail.com>
2016-04-06 14:02:58 -0700

    fixed the escaping commas to include case if quote isn't right before/after comma

--- JamesonPractice/parseCSV.py
+++ JamesonPractice/parseCSV.py
@@ -10,19 +10,26 @@ def parse_csv(text):
 	# keep track of beginning of "print" and an idx that moves
 	beg_idx = 0
 	curr_idx = 0
+	quote = False
 
 	while curr_idx < len(text):
-		if text[curr_idx] == ",":
-			# if we've escaped
-			if text[curr_idx-1] == "\"" and text[curr_idx+1] == "\"":
+		# if we see a quote, we need to ignore commas until we see another so set to True
+		if text[curr_idx] == "\"" and not quote:
+			quote = True
+			curr_idx += 1
+		# if we've seen a quote already, we have our second "escape" quote so return to false so we catch next comma as seperator
+		elif text[curr_idx] == "\"" and quote:
+			quote = False
+			curr_idx +=1
+
+		# if we hit a comma and we don't have a quote/it's not within an escape, print the string so far
+		elif text[curr_idx] == "," and not quote:
+			print text[beg_idx:curr_idx].replace("\",\"", ",").strip().strip("\"")
+			beg_idx = curr_idx+1
+			curr_idx = beg_idx
+		
+		elif text[curr_idx] == "," and quote:
 				curr_idx += 1
-			else:
-				# replace any escaped commas you found with a comma
-				out = text[beg_idx:curr_idx].replace("\",\"", ",")
-				print out.strip()
-				# move the beg_index to one past the current(aka the comma)
-				beg_idx = curr_idx+1
-				curr_idx = beg_idx
 		else:
 			curr_idx += 1
 
@@ -30,7 +37,18 @@ def parse_csv(text):
 	print out.strip()
 
 if __name__ == '__main__':
+	## input: 
+	#rocky,ruby","dog,"one,two",yes
+	#escape, all, the things
+	# output in terminal:
+		# rocky
+		# ruby,dog
+		# one,two
+		# yes
+		# escape
+		# all
+		# the things
+
 	f_info = open("test.csv")
 
 	parse_csv(f_info.read())
-	# parse_csv("rocky,ruby\",\"dog")
